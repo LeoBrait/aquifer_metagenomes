2abb53d6466ee808c721f1971538130fafcccaf2
Leonardo Brait <leonardobrait@gmail.com>
2022-11-06 12:53:58 -0300

    readme.md insertion

---
+++ README.md
@@ -0,0 +1,17 @@
+########################### Read Me ##########
+#Authors: Brait LAS, Barbosa FAS, Ferreira CAM, 
+#Mkrull and Moreira EF.
+###############################################
+#this is a script made to analyse public metage
+#nomic annotated data from aquifer microbiomes.
+#Here we do Simper, CCA, NMDS, Anovas, peranovas
+#and permanovas to reveal patterns of functional
+#and taxaonomic structure and diversity of
+#different aquifer types.
+###############################################
+# Revised by: Meirelles, PM
+# on: 05/11/2022
+###############################################
+# First submission on: 05/11/2022
+# Last Update on:
+###############################################
--- pipelines/master.r
+++ pipelines/master.r
@@ -1,22 +1,3 @@
-########################### Read Me ##########
-#Authors: Brait LAS, Barbosa FAS, Ferreira CAM, 
-#Mkrull and Moreira EF.
-###############################################
-#this is a script made to analyse public metage
-#nomic annotated data from aquifer microbiomes.
-#Here we do Simper, CCA, NMDS, Anovas, peranovas
-#and permanovas to reveal patterns of functional
-#and taxaonomic structure and diversity of
-#different aquifer types.
-###############################################
-# Revised by: Meirelles, PM
-# on: 05/11/2022
-###############################################
-# First submission on: 05/11/2022
-# Last Update on:
-###############################################
-
-
 ############################# SETWD ##########
 ##############################################
 #Description: Please, change the path of the
